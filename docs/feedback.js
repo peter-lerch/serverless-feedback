9b58336f3c1ede6ad83b9f929dd09e1dc2318baa
peter-lerch <55148127+peter-lerch@users.noreply.github.com>
2019-09-13 12:09:46 +0200

    Update feedback.js

--- docs/feedback.js
+++ docs/feedback.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/07-feedback';
+const apiUrl = 'https://d526c02f.eu-gb.apiconnect.appdomain.cloud/22-feedback';
 const feedback = {
   // retrieve the existing feedback entries
   get() {
